24ea471c9eed78a7bba3987f316d5f39bcb791b5
chulhee23 <chulhee23@naver.com>
2019-06-16 12:02:34 +0900

    조정

--- app/models/team.rb
+++ app/models/team.rb
@@ -8,8 +8,8 @@ class Team < ApplicationRecord
     has_many :votes
     has_many :votants, through: :votes, source: :user 
     def short_title
-        if (self.title.length > 10)
-            return self.title[0..10]+'...'
+        if (self.title.length > 11)
+            return self.title[0..11]+'...'
         else
             return self.title
         end
