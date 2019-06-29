3f72931961d829f6021e425538de7721f0c60c90
chulhee23 <chulhee23@naver.com>
2019-06-29 14:13:15 +0900

    test

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -23,6 +23,15 @@ index do
   column :created_at
   actions
 end
+form do |f|
+  f.inputs do 
+    f.input :email
+    f.input :name
+    f.input :univ
+    f.input :password
+  end
+  actions 
+end
 
 # 필터링
 filter :email
